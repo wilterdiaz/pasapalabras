f16ec4a138f2fb3b2ccd708808cccf7f87ab65a3
WilterD <wilterd18@gmail.com>
2024-10-19 17:26:45 -0400

    mejoras seleccion

---
+++ src/Componets/AlphabetRoulette.jsx
@@ -0,0 +1,46 @@
+// AlphabetRoulette.jsx
+import React, { useState } from 'react';
+import './AlphabetRoulette.css';
+
+function AlphabetRoulette() {
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const [selectedLetters, setSelectedLetters] = useState([]);
+
+  const handleLetterClick = (letter) => {
+    setSelectedLetters(prevSelectedLetters =>
+      prevSelectedLetters.includes(letter)
+        ? prevSelectedLetters.filter(l => l !== letter)
+        : [...prevSelectedLetters, letter]
+    );
+  };
+
+  const resetRoulette = () => {
+    setSelectedLetters([]);
+  };
+
+  return (
+    <div className="roulette-container">
+      <div className="roulette">
+        {letters.map((letter, index) => (
+          <div
+            key={letter}
+            className={`letter ${selectedLetters.includes(letter) ? 'selected' : ''}`}
+            style={{
+              transform: `
+                rotate(${(360 / letters.length) * index}deg)
+                translate(0, -150px)
+                rotate(-${(360 / letters.length) * index}deg)
+              `
+            }}
+            onClick={() => handleLetterClick(letter)}
+          >
+            {letter}
+          </div>
+        ))}
+      </div>
+      <button onClick={resetRoulette}>Reiniciar</button>
+    </div>
+  );
+}
+
+export default AlphabetRoulette;
